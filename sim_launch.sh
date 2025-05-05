b2c9c2e3b68df340121cfa26dae65c5cd4673e03
enricocovili <henrycovili2004@gmail.com>
2025-05-05 15:48:28 +0200

    changed params + launch script

--- sim_launch.sh
+++ sim_launch.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+ROOT_DIR="$HOME/Formula"
+
+# Check if "--use-cpu-clustering" is passed as an argument
+USE_CPU_CLUSTERING=false
+for arg in "$@"; do
+    if [ "$arg" == "--use-cpu-clustering" ]; then
+        USE_CPU_CLUSTERING=true
+        break
+    fi
+done
+
 # Source files from mmr-drive, orin-drive, and gazebo-sim
-source ~/Formula/mmr-drive/install/setup.sh
-source ~/Formula/orin-drive/install/setup.sh
-source ~/Formula/gazebo-sim/install/setup.sh
-source ~/Formula/pcl-filter/install/setup.sh
+if [ "$USE_CPU_CLUSTERING" == true ]; then
+    source $ROOT_DIR/mmr-drive/install/setup.sh
+else
+    source $ROOT_DIR/clustering_plane_finder_gpu/install/setup.sh
+fi
+source $ROOT_DIR/orin-drive/install/setup.sh
+source $ROOT_DIR/gazebo-sim/install/setup.sh
+source $ROOT_DIR/pcl-filter/install/setup.sh
 
 # Start a new tmux session and run the commands in separate windows
 tmux new-session -d -s simulation "ros2 launch gazebo-sim simulator_launch.py"
 tmux split-window -v -t simulation "ros2 launch pcl-filter filter_launch.py"
-tmux split-window -h -t simulation "ros2 launch clustering_plane_finder_cpu clustering_plane_finder_cpu_launch.py"
-tmux split-window -v -t simulation "ros2 launch marker_adapter marker_adapter.launch.py"
-tmux split-window -h -t simulation "ros2 launch mmr_ekf_odometry simulation.launch.py"
+if [ "$USE_CPU_CLUSTERING" == true ]; then
+    tmux split-window -h -t simulation "ros2 launch clustering_plane_finder_cpu clustering_plane_finder_cpu_launch.py"
+fi
+tmux split-window -v -t simulation "ros2 launch mmr_ekf_odometry simulation.launch.py"
+tmux split-window -h -t simulation "ros2 launch local_planner local_planner.launch.py"
+
 tmux select-layout -t simulation tiled
 
 tmux attach-session -t simulation
